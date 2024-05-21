b380a0ff6ef902a5d621343db81caceeca56e137
ericsaza <eric.saladozafra@gmail.com>
2024-05-21 17:16:26 +0200

    Añadido endpoint de últimos animes añadidos

--- animes/animeflv/animeflv_api.py
+++ animes/animeflv/animeflv_api.py
@@ -4,6 +4,8 @@ from scrapper.utils import obtener_contenido_url
 
 
 class AnimeflvAPI:
+    
+    # Endpoint para ver los últimos episodios de animes
     def episodios_recientes(self):
         
         # Entramos a la página donde scrapearemos la información
@@ -23,3 +25,25 @@ class AnimeflvAPI:
             lista_animes.append({"name": titulo, "episode": episodio, "image_src": image, "url": url})
         
         return {"message": "Últimos episodios de animes", "data": lista_animes, "code": 200}
+    
+    # Endpoint para ver los últimos animes añadidos
+    def mostrar_ultimos_animes(self):
+        
+        # Parsea el contenido HTML de la página
+        soup = obtener_contenido_url(f"https://www3.animeflv.net")
+        
+        # Buscamos todos los animes recientes
+        lista_animes = []
+        for anime in soup.find("ul", {"class": "ListAnimes"}).find_all("li"):
+            titulo = anime.find("h3").text
+            image = f'https://www3.animeflv.net{anime.find("img")["src"]}'
+            sinopsis = anime.find_all("p")[1].text
+            info_anime = f'https://www3.animeflv.net{anime.find_all("a")[1]["href"]}'
+            anime_type = anime.find("span", {"class": "Type"}).text
+            url_api = anime.find_all("a")[1]["href"].replace("/anime/", "/buscar_anime?anime_a_buscar=")
+            rating = anime.find("span", {"class": "Vts fa-star"}).text
+            
+            # Agregamos los datos a la lista
+            lista_animes.append({"title": titulo, "image_src": image, "sinopsis": sinopsis, "view_info": info_anime, "type": anime_type, "url_api": url_api, "puntuacion": rating})
+        
+        return {"message": "Últimos animes añadidos", "data": lista_animes, "code": 200}
--- animes/animeflv/routes.py
+++ animes/animeflv/routes.py
@@ -8,10 +8,20 @@ router = APIRouter()
 # Instanciamos la clase PrincipalAPI
 animeflv_api = AnimeflvAPI()
 
+# Endpoint para obtener los últimos episodios de animes
 router.add_api_route(
-    path="/",
+    path="/episodios-recientes",
     endpoint=animeflv_api.episodios_recientes,
     methods=["GET"],
     description="Ver los útimos episodios agregados en AnimeFLV.",
     name="Episodios recientes",
+)
+
+# Endpoint para obtener los últimos animes añadidos
+router.add_api_route(
+    path="/ultimos-animes",
+    endpoint=animeflv_api.mostrar_ultimos_animes,
+    methods=["GET"],
+    description="Ver los últimos animes añadidos en AnimeFLV.",
+    name="Últimos animes",
 )
